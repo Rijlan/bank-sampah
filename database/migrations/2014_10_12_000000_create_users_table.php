bf84e2eaf4ca20d467fa31d3a15f7fdff0d03759
RijlanM10 <rijlan01@gmail.com>
2020-12-21 16:24:55 +0700

    menambah migrasi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('telpon')->unique();
+            $table->text('alamat');
+            $table->string('foto')->nullable();
+            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_12_21_091802_create_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('from');
+            $table->unsignedBigInteger('to');
+            $table->text('pesan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
