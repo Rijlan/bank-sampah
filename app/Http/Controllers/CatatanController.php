5fe7d4da1ab2fa30ab7a725366042ce52ee7f109
RijlanM10 <rijlan01@gmail.com>
2021-01-02 17:29:10 +0700

    menambahkan total

--- app/Http/Controllers/CatatanController.php
+++ app/Http/Controllers/CatatanController.php
@@ -40,11 +40,32 @@ class CatatanController extends Controller
 
     public function total()
     {
+        // total sampah yang pernah didata
         $total = DB::select(DB::raw("SELECT jenis_sampahs.jenis, SUM(berat) AS berat FROM catatans LEFT JOIN jenis_sampahs ON catatans.jenis_sampah_id = jenis_sampahs.id GROUP BY jenis_sampahs.jenis"));
 
-        // dd($total);
-        $peringkat = DB::select(DB::raw("SELECT users.name, SUM(berat) AS total FROM users LEFT JOIN catatans ON users.id = catatans.user_id WHERE users.role = 3 GROUP BY users.name ORDER BY total DESC LIMIT 5"));
+        // total sampah yang sudah dijual
+        $sudah = DB::select(DB::raw("SELECT jenis_sampahs.jenis, SUM(berat) AS berat FROM penjualans LEFT JOIN jenis_sampahs ON penjualans.jenis_sampah_id = jenis_sampahs.id GROUP BY jenis_sampahs.jenis"));
 
-        // dd($peringkat);
+        
+        // total sampah yang belum dijual
+        $arr = [];
+        foreach ($total as $key => $value) {
+            $arr[$key]['jenis'] = $value->jenis;
+            if (empty($sudah[$key])) {
+                $arr[$key]['berat'] = $value->berat;
+            } else {
+                $arr[$key]['berat'] = $value->berat - $sudah[$key]->berat;
+            }
+        }
+
+        $belum = [];
+        foreach ($arr as $to_obj) {
+            $belum[] = (object) $to_obj;
+        }
+
+        // 5 peringkat
+        $peringkat = DB::select(DB::raw("SELECT users.name, SUM(berat) AS total_berat, SUM(jenis_sampahs.harga_nasabah * berat) AS total_harga FROM users LEFT JOIN catatans ON users.id = catatans.user_id LEFT JOIN jenis_sampahs ON catatans.jenis_sampah_id = jenis_sampahs.id WHERE users.role = 3 GROUP BY users.name ORDER BY total_harga DESC LIMIT 5"));
+
+        dd($total, $sudah, $belum, $peringkat);
     }
 }
